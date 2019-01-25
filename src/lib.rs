68ee96f1664f3407a7cb7ff4c34c0a19b110a57f
Michael Goin <mgoin@vols.utk.edu>
2019-01-25 18:43:40 -0500

    Added files from advos module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![feature(panic_info_message,allocator_api,asm,lang_items,compiler_builtins_lib)]
+//We are not permitted to use the standard library since it isn't written for our operating system
+#![no_std]
+
+//The eh_personality tells our program how to unwind. We aren't going to write that, so tell
+//it to do nothing.
+#[lang = "eh_personality"]
+pub extern fn eh_personality() {}
+
+//Abort will be used when panic can't
+#[no_mangle]
+fn abort() -> !
+{
+   loop {}
+}
+
+//Panic handler will execute whenever our rust code panics. -> ! means that this function won't return,
+//so we have to make sure it doesn't.
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    abort()
 }
