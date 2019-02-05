025a75e07dfc2568bf9f3ad15f9a07a4f6875513
Jonathan Ambrose <jambros2@vols.utk.edu>
2019-02-05 13:26:06 -0500

    Added additional comments throughout the file to clearly state what's going on

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//Michael Goin, Jacob Rutherford, Jonathan Ambrose
+//2-5-2019
+//This is the main library of the os. In this, init,
+//readchar, and writechar are all functions for the UART.
+
 #![feature(panic_info_message,allocator_api,asm,lang_items,compiler_builtins_lib)]
 //We are not permitted to use the standard library since it isn't written for our operating system
 #![no_std]
@@ -23,6 +28,9 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     abort()
 }
 
+//Constants defined for the UART. The clock frequency
+//is set with features during compilation
+
 #[cfg(feature = "qemu")]
     const CLOCK_FREQ: u64 = 65_000_000; // QEMU Frequency
 #[cfg(feature = "hifive")]
@@ -32,14 +40,17 @@ const DIVISOR: u64 = (CLOCK_FREQ / BAUD_RATE) - 1;
 
 const UART_ADDR: u64 = 0x1001_3000;
 const TXDATA: u64 = UART_ADDR + 0x000; // Transmit data register
-const RXDATA: u64 = UART_ADDR + 0x004; // Recieve data register
+const RXDATA: u64 = UART_ADDR + 0x004; // Receive data register
 const TXCTRL: u64 = UART_ADDR + 0x008; // Transmit control register
-const RXCTRL: u64 = UART_ADDR + 0x00c; // Recieve control register
+const RXCTRL: u64 = UART_ADDR + 0x00c; // Receive control register
 const IE:     u64 = UART_ADDR + 0x010; // UART interrupt enable
 const IP:     u64 = UART_ADDR + 0x014; // UART interrupt pending
 const DIV:    u64 = UART_ADDR + 0x018; // Baud rate divisor
 
 use core::ptr::{read_volatile, write_volatile};
+
+//This initializes the UART by setting the transmit enable and read enable bits
+//as well as the divisor
 
 pub fn init() -> () {
     let div = DIV as *mut u32;
@@ -50,10 +61,13 @@ pub fn init() -> () {
         write_volatile(div, DIVISOR as u32 & 0x0000_FFFF);
         // Enable transmission by setting first bit in txctrl register
         write_volatile(txctrl, read_volatile(txctrl) | 1);
-        // Enable recieving by setting first bit in rxctrl register
+        // Enable receiving by setting first bit in rxctrl register
         write_volatile(rxctrl, read_volatile(rxctrl) | 1);
     }
 }
+
+//This function reads a single character if it is available
+//It returns none if one is not available
 
 pub fn readchar() -> Option<u8> {
     let rxdata = RXDATA as *mut u32;
@@ -66,6 +80,9 @@ pub fn readchar() -> Option<u8> {
     // Otherwise, return the char
     else { Some(r as u8) }
 }
+
+//This function writes a single char. It will block until
+//the FIFO has space for it. Then it will write it
 
 pub fn writechar(byte: u8) -> () {
     let txdata = TXDATA as *mut u32;
