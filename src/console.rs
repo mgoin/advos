030b5ed7eeca109895563b959e7113e6e2edf9e2
Jacob Rutherford <jruthe10@vols.utk.edu>
2019-02-07 11:48:53 -0500

    Console: Implement read and write
    Console::{read,write} are interfaces to the UART driver for
    print! and println! macros. Currently, these do a little bit of
    buffering on reading from the UART, looking for newline characters and
    appending them to the strings that are returned.

--- src/console.rs
+++ src/console.rs
@@ -4,7 +4,41 @@ pub mod uart;
 
 pub struct Console;
 
-impl core::fmt::write for Console {
+impl core::fmt::Write for Console {
     fn write_str(&mut self, s: &str) -> Result<(), Error> {
+        Console::write(s)
+    }
+}
+
+impl Console {
+    pub fn write(s: &str) -> Result<(), Error> {
+        for c in s.chars() {
+            uart::writechar(c as u8);
+        }
+
+        Ok(())
+    }
+
+    pub fn read() -> Result<(), Error> {
+        let mut ret_str: [char; 256] = ['a'; 256]; // fill the buffer with a filler letter
+        let mut next_char_index = 0;
+        while let Some(c) = uart::readchar() {
+            if c.is_ascii_control() {
+                match c as char {
+                    '\n' =>
+                    {
+                        ret_str[next_char_index] = c as char;
+                        Ok(())
+                    },
+                    _ =>
+                    {
+                        Err("Got invalid char!")
+                    },
+                }.unwrap();
+            }
+            ret_str[next_char_index] = c as char;
+            next_char_index += 1;
+        }
+        Ok(())
     }
 }
