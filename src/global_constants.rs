55b35d4b6cab4290cb71dbf3db2ae5b5e8f4573e
Jacob Rutherford <jruthe10@vols.utk.edu>
2019-02-22 13:28:41 -0500

    Timer: Now works as expected
    Per Dr. Marz' suggestion, the mtime_cmp_{hi,lo} values should be
    incremented individually of eachother instead of shoving them both into
    a u64. Once they are incremented with the constant offset, they are
    rewritten to the CLINT.
    
    Fixed a bug where mtime_cmp_lo was being written to with the value for
    time_cmp_lo and then written again with the value of time_cmp_hi.

--- src/global_constants.rs
+++ src/global_constants.rs
@@ -1,5 +1,5 @@
 // Master clock frequency in hertz
-pub const CLOCK_FREQ: u64 = 65_000_000;
+pub const CLOCK_FREQ: u64 = 10_000_000;
 
 // Address of the UART for mmio
 pub const UART_ADDR: u64 = 0x1001_3000;
--- src/trap/timer.rs
+++ src/trap/timer.rs
@@ -5,7 +5,7 @@ use core::fmt::{Write,Error};
 use core::ptr::{write_volatile, read_volatile};
 
 const CTX_PER_SECOND    : u64 = 1;
-const TIME_TO_CTX_SWITCH: u64 = CLOCK_FREQ / CTX_PER_SECOND;
+const TIME_TO_CTX_SWITCH: u32 = (CLOCK_FREQ / CTX_PER_SECOND) as u32;
 
 const MTIME_CMP_LO: u64 = CORE_LOCAL_INTERRUPT_MAP + 0x4000;
 const MTIME_CMP_HI: u64 = CORE_LOCAL_INTERRUPT_MAP + 0x4004;
@@ -13,22 +13,24 @@ const MTIME_LO    : u64 = CORE_LOCAL_INTERRUPT_MAP + 0xBFF8;
 const MTIME_HI    : u64 = CORE_LOCAL_INTERRUPT_MAP + 0xBFFC;
 
 pub fn init() -> Result<(), Error> {
-  println!("time to ctx switch: {}", TIME_TO_CTX_SWITCH);
-  write_time_cmp(TIME_TO_CTX_SWITCH);
-
-  Ok(())
+  incr()
 }
 
 pub fn incr() -> Result<(), Error> {
-  let mut time: u64 = 0;
-  let (time_lo, time_hi) = read_mtime();
+  let (mut time_lo, mut time_hi) = read_mtime();
 
-  time |= (time_lo & 0xFFFFFFFF) as u64;
-  time |= ((time_hi & 0xFFFFFFFF) as u64) << 32;
+  println!("timer = {}", time_lo);
 
-  time = time.wrapping_add(TIME_TO_CTX_SWITCH);
+  let prev_time = time_lo;
+  time_lo = time_lo.wrapping_add(TIME_TO_CTX_SWITCH);
 
-  write_time_cmp(time);
+  println!("new timer = {}", time_lo);
+
+  if time_lo < prev_time {
+    time_hi += 1;
+  }
+
+  write_time_cmp(time_lo, time_hi);
 
   Ok(())
 }
@@ -46,11 +48,11 @@ fn read_mtime() -> (u32, u32) {
   (time_lo, time_hi)
 }
 
-fn write_time_cmp(time: u64) -> () {
+fn write_time_cmp(time_lo: u32, time_hi: u32) -> () {
   let cmp_lo_addr = MTIME_CMP_LO as *mut u32;
   let cmp_hi_addr = MTIME_CMP_HI as *mut u32;
   unsafe {
-    write_volatile(cmp_lo_addr, (time & 0xFFFFFFFF) as u32);
-    write_volatile(cmp_lo_addr, ((time >> 32) & 0xFFFFFFFF) as u32);
+    write_volatile(cmp_lo_addr, time_lo);
+    write_volatile(cmp_hi_addr, time_hi);
   }
 }
