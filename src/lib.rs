62e23681b2aa84137dc1b6846a94d743403e0aff
Jonathan Ambrose <jambros2@vols.utk.edu>
2019-02-28 00:52:55 -0500

    Added start of memory manager code. Coalesce does not appear to be working right. I think there are some length values changing that I know I set correctly

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,10 @@ mod console;
 mod global_constants;
 mod trap;
 mod lock;
+mod memman;
 
 use console::Console;
+use memman::MemManager;
 use core::fmt::Write;
 
 //The print! macro will print a string by calling write!
@@ -69,15 +71,24 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     abort()
 }
 
-#[no_mangle]
-fn main() {
-    unsafe { enable_interrupts(); }
-    println!("interrupts enabled");
+fn test_mutex() -> () {
+    let mut m = lock::Mutex::new();
+    println!("Locking mutex..."); m.lock();
+    println!("Unlocking mutex..."); m.unlock();
+    println!("Locking mutex again..."); m.lock();
+}
 
-    // Intialize UART for reading/writing
-    console::uart::init().unwrap();
+fn echo_from_console() -> () {
+    if let Some(s) = console::Console::read() {
+        print!("\r\nread \"");
+        for c in s.iter() {
+            print!("{}", c);
+        }
+        println!("\" from uart");
+    }
+}
 
-    // Test lines for formatting with println!
+fn test_println() -> () {
     println!();
     println!("Test lines: ");
     println!("  Lowercase Hex: 15 = {:x}", 15);
@@ -87,12 +98,134 @@ fn main() {
     println!("  Formatted Double: 1.23456 of width 3 is {:.3}", 1.23456);
     println!("  Formatted Int: 42 of width 4 with leading zeroes is {:04}", 42);
     println!();
+}
+
+#[no_mangle]
+fn test_memman() -> () {
+    unsafe {
+        MemManager::init();
+
+        //Allocate an 16 byte quantity
+
+        let p = MemManager::kmalloc(16).unwrap();
+        let pnt = p as *mut u32;
+        println!("Allocated 16 bytes at Address {:}", p);
+        println!("Value from pnt {:}", *pnt);
+        *pnt = 12;
+        println!("Value from pnt {:}", *pnt);
+        println!();
+
+        //Allocate an 8-byte quantity
+
+        let pt = MemManager::kmalloc(8).unwrap();
+        let pts = pt as *mut u32;
+        println!("Allocated 8 bytes at Address {:}", pt);
+        println!("Value from pnt {:}", *pts);
+        *pts = 8;
+        println!("Value from pnt {:}", *pts);
+        println!();
+
+        //Allocate a 24 byte quantity
+
+        let pt1 = MemManager::kmalloc(24).unwrap();
+        let pt1s = pt1 as *mut u32;
+        println!("Allocated 24 bytes at Address {:}", pt1);
+        println!("Value from pnt {:}", *pt1s);
+        *pt1s = 4;
+        println!("Value from pnt {:}", *pt1s);
+        println!();
+
+        //Free the middle quantity that is 8 bytes
+
+        MemManager::kfree(pt).unwrap();
+        println!("Freed Address {:}", pt);
+        println!();
+
+        //Allocate a 24 byte quantity to show that
+        //it won't take the 8 byte quantity in the middle
+
+        let pt = MemManager::kmalloc(24).unwrap();
+        let pts = pt as *mut u32;
+        println!("Allocated 24 bytes at Address {:}", pt);
+        println!("Value from pnt {:}", *pts);
+        *pts = 3;
+        println!("Value from pnt {:}", *pts);
+        println!();
+
+        //Now show that a small enough block will take it
+
+        let pt2 = MemManager::kmalloc(4).unwrap();
+        let pt2s = pt2 as *mut u32;
+        println!("Allocated 4 bytes at Address {:}", pt2);
+        println!("Value from pnt {:}", *pt2s);
+        *pt2s = 3;
+        println!("Value from pnt {:}", *pt2s);
+        println!();
+
+        //Free them all
+
+        MemManager::kfree(p).unwrap();
+        MemManager::kfree(pt1).unwrap();
+        MemManager::kfree(pt2).unwrap();
+        MemManager::kfree(pt).unwrap();
+        println!("Freed Address {:}", p);
+        println!("Freed Address {:}", pt);
+        println!("Freed Address {:}", pt1);
+        println!("Freed Address {:}", pt2);
+        println!();
+
+        //Show that fragmentation doesn't let this go at the front
+
+        let pt = MemManager::kmalloc(24).unwrap();
+        let pts = pt as *mut u32;
+        println!("Allocated 24 bytes at Address {:}", pt);
+        println!("Value from pnt {:}", *pts);
+        *pts = 17;
+        println!("Value from pnt {:}", *pts);
+        println!();
+
+        //Free it, coalesce, and show it will go at the front
+
+        MemManager::kfree(pt).unwrap();
+        println!("Freed Address {:}", pt);
+        println!();
+        MemManager::kcoalesce();
+        println!("Coalesced Free List");
+        println!();
+        let pt = MemManager::kmalloc(24).unwrap();
+        let pts = pt as *mut u32;
+        println!("Allocated 24 bytes at Address {:}", pt);
+        println!("Value from pnt {:}", *pts);
+        *pts = 14;
+        println!("Value from pnt {:}", *pts);
+        println!();
+
+        MemManager::kfree(pt).unwrap();
+        println!("Freed Address {:}", pt);
+        println!();
+    }
+}
+
+#[no_mangle]
+fn main() {
+    unsafe { enable_interrupts(); }
+    println!("interrupts enabled");
+
+    // Intialize UART for reading/writing
+    console::uart::init().unwrap();
+
+    // Test lines for formatting with println!
+    test_println();
 
     // Test mutex locking and unlocking
+    //test_mutex();
     let mut m = lock::Mutex::new();
     println!("Locking mutex..."); m.lock();
     println!("Unlocking mutex..."); m.unlock();
     println!("Locking mutex again..."); m.lock();
+
+    // Test the Memory Manager
+    test_memman();
 
     let clim = global_constants::CORE_LOCAL_INTERRUPT_MAP as *mut u32;
     let interrupt_mask: u32 = 0x008;
@@ -104,12 +237,6 @@ fn main() {
     trap::timer::init().unwrap();
 
     loop {
-        if let Some(s) = console::Console::read() {
-            print!("\r\nread \"");
-            for c in s.iter() {
-                print!("{}", c);
-            }
-            println!("\" from uart");
-        }
+        echo_from_console();
     }
 }
---
+++ src/memman/mod.rs
@@ -0,0 +1,192 @@
+//Michael Goin, Jacob Rutherford, and Jonathan Ambrose
+//2-26-2019
+//This code implements the memory manager.
+
+//use core::fmt::Error;
+use core::ptr::{read_volatile, write_volatile};
+
+/*
+extern "C" {
+    static __heap_start: u32;
+    static __heap_end: u32;
+}
+*/
+const HEAP_START: u64 = 0x80000000;
+const HEAP_END: u64 = 0x80003800;
+
+pub struct Descriptor {
+    len: u16,
+    taken: u16
+}
+
+pub struct MemManager;
+
+impl MemManager {
+
+    #[no_mangle]
+
+    //Initialize by setting the first descriptor at the start of the heap
+
+    pub fn init() -> () {
+
+        unsafe{
+            let desc = HEAP_START as *mut Descriptor;
+            write_volatile(&mut ((*desc).len), (HEAP_END - HEAP_START) as u16);
+            write_volatile(&mut ((*desc).taken), 0);
+        }
+    }
+
+    //Mallocs a block of memory. Returns a Result
+    //Ok has a u32 in it upon return corresponding to
+    //the address of the memory
+    //Error will return a string with the error message
+
+    #[no_mangle]
+    pub fn kmalloc(sz: usize) -> Result<u32, &'static str> {
+
+        unsafe {
+            let mut start = HEAP_START as u32;
+            let end = HEAP_END as u32;
+            let mut pnt = 0 as u32;
+
+            //Get the size to a multiple of 4
+
+            let size = (sz as u32) + 3 - (sz as u32 - 1)%4 as u32;
+
+            //Start at heap_start
+            //Go until we reach the end or break
+
+            while start != end {
+
+                //Make a descriptor at the start
+
+                let desc = start as *mut Descriptor;
+
+                //Check if we're taken
+
+                let t = read_volatile(&((*desc).taken)) as u16;
+                if t != 1 {
+
+                    //Check if it's large enough
+
+                    let s = read_volatile(&((*desc).len)) as u16;
+                    if s as u32 >= size+core::mem::size_of::<Descriptor>() as u32 {
+
+                        //If it is big enough, mark as taken and reset the size
+
+                        write_volatile(&mut ((*desc).taken), 1 as u16);
+                        write_volatile(&mut ((*desc).len), (size+core::mem::size_of::<Descriptor>() as u32) as u16);
+
+                        //Make a new descriptor. Check if it is taken already (could've
+                        //allocated the perfect amount to fill a block). Only do this if
+                        //we wouldn't be at the end
+
+                        if start+size+core::mem::size_of::<Descriptor>() as u32 != end {
+                            let new_desc = (start + size + (core::mem::size_of::<Descriptor>() as u32)) as *mut Descriptor;
+                            let new_taken = read_volatile(&((*new_desc).taken)) as u16;
+                            if new_taken != 1 {
+                                write_volatile(&mut ((*new_desc).taken), 0 as u16);
+                                write_volatile(&mut ((*new_desc).len), s - read_volatile(&((*desc).len)) as u16);
+                            }
+                        }
+
+                        //Set the pointer that we'll return
+
+                        pnt = start + core::mem::size_of::<Descriptor>() as u32;
+                        break;
+                    }
+                }
+
+                //If we didn't break, we set the location of the new Descriptor
+
+                start = start + read_volatile(&((*desc).len)) as u32;
+            }
+
+            //If the pointer isn't 0, we return it
+
+            if pnt !=0 {
+                Ok(pnt)
+            }
+
+            //Otherwise, return an error
+
+            else {
+                Err("Not enough memory")
+            }
+        }
+    }
+
+    //This function frees a given pointer
+    //It returns an empty Ok or and Err string
+
+    pub fn kfree(p: u32) -> Result<(), &'static str> {
+        let mut badpnt = 0 as u16;
+        unsafe {
+            let desc = (p - core::mem::size_of::<Descriptor>() as u32) as *mut Descriptor;
+
+            //If the pointer isn't taken, it's a bad pointer
+
+            if read_volatile(&((*desc).taken)) != 1 {
+                badpnt = 1;
+            }
+            else {
+
+                //Set the descriptor to not be taken
+
+                write_volatile(&mut ((*desc).taken), 0 as u16);
+            }
+        }
+        if badpnt == 0 {
+            Ok(())
+        }
+        else {
+            Err("Bad pointer")
+        }
+    }
+
+    //This function coalesces the memory, bringing together contiguous
+    //elements that are not taken
+
+    #[no_mangle]
+    pub fn kcoalesce() -> () {
+
+        unsafe {
+            let mut start = HEAP_START as u32;
+            let end = HEAP_END as u32;
+            let mut next: u32 = start;
+
+            //Start at heap_start
+            //Go until we reach the end or break
+
+            while next != end {
+
+                //Make a descriptor at the start
+
+                let desc = start as *mut Descriptor;
+
+                //Find where the next descriptor will be
+
+                next = (start + read_volatile(&((*desc).len)) as u32) as u32;
+
+                //If we aren't at the end, try to merge
+
+                if next != end {
+                    let desc2 = next as *mut Descriptor;
+
+                    //Check if either are taken. If neither is taken, merge them
+
+                    let t = read_volatile(&((*desc).taken)) as u16;
+                    let t2 = read_volatile(&((*desc2).taken)) as u16;
+
+                    if t != 1 && t2 != 1 {
+                        write_volatile(&mut ((*desc).len), read_volatile(&((*desc).len)) + read_volatile(&((*desc2).len)));
+                    }
+                    else {
+                        //If we couldn't merge, move to the next element
+                        start = next;
+                    }
+                }
+            }
+        }
+    }
+}
