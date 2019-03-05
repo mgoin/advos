41187339a1abddc2b095fec649bfffcb6b137c6b
Jacob Rutherford <jruthe10@vols.utk.edu>
2019-03-04 19:13:37 -0500

    Lock: Address comments left by Dr. Marz for locking
    Our asm! call was overly complicated and created a label that wasn't
    necessary. We can achieve the same result using rust and only using asm!
    for making the one amoswap.w.aq call, similarly to the amoswap.w.rl
    call.

--- src/lock/mod.rs
+++ src/lock/mod.rs
@@ -12,13 +12,15 @@ impl Mutex {
   
   // Tries to lock the mutex, blocking until it can do so
   pub fn lock(&mut self) {
-    unsafe {
-      asm!("
-          li t0, 1                  # Initialize swap value
-        again:
-          amoswap.w.aq t0, t0, ($0) # Attempt to acquire lock
-          bnez t0, again            # Retry if held"
-        :: "r"(&self.state) : "t0" : "volatile");
+    let mut test = 1;
+
+    loop {
+      unsafe { asm!("amoswap.w.aq $0, $0, ($1)"
+                    : "+r"(test) : "r"(&self.state) :: "volatile");
+      }
+      if test == 0 {
+        return;
+      }
     }
   }
   
