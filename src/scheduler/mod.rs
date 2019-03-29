1798482c3bb4ce529c01c535584ffc21f5475b7c
Michael Goin <mgoin@vols.utk.edu>
2019-03-29 13:21:40 -0400

    Scheduler: initial round robin

--- src/scheduler/mod.rs
+++ src/scheduler/mod.rs
@@ -6,27 +6,51 @@ pub mod pcb;
 
 // The fixed amount of time each process runs before the
 // round-robin scheduler switches to another process
-const TIME_QUANTUM: u32 = 10000;
+const TIME_QUANTUM: usize = 10000;
 
 pub struct Scheduler {
+    current_index: usize,
     processes: heapvec::HeapVec<pcb::ProcessControlBlock>,
 }
 
 impl Scheduler {
     // Creates a Scheduler with an init process
     pub fn new() -> Scheduler {
-        let mut s = Scheduler { processes: heapvec::HeapVec::new(32) };
+        let mut s = Scheduler { current_index: 0,
+                                processes: heapvec::HeapVec::new(32) };
         // Create the init process as the first process
-        s.processes.push(pcb::ProcessControlBlock::new_init());
+        s.processes.push(pcb::ProcessControlBlock::new(0, 0, None, 0));
         return s;
     }
 
-    pub fn save_pcb(self, process_id: usize, mepc: u32) -> u32 {
+    pub fn context_switch(self) {
+        // Pick a process to switch to using the scheduling algorithm
+        // Round Robin
+        let i = self.current_index + 1;
+        while i != self.current_index {
+            if i == self.processes.size() - 1 {
+                i = 0;
+            }
+            else if self.processes[i].state != pcb::ProcessState::Running {
+                i += 1;
+            }
+            else {
+                break;
+            }
+        }
+        let new_index = i;
+
+        // Switch registers 
+
+        // Done??
+    }
+
+    pub fn save_pcb(self, process_id: usize, mepc: usize) -> usize {
         let p = self.processes[process_id];
         for i in 0..pcb::NUM_CPU_REGISTERS {
             p.registers[i] = read_volatile(GLOBAL_CTX + i);
         }
-        p.state = pcb::ProcessState::Waiting;
+        p.state = pcb::ProcessState::Running;
         p.program_counter = mepc;
         return 1;
     }
--- src/scheduler/pcb.rs
+++ src/scheduler/pcb.rs
@@ -2,12 +2,12 @@ use crate::global_constants::*;
 
 use crate::trap::timer;
 
+#[derive(PartialEq)]
 pub enum ProcessState {
-    Start,
-    Ready,
-    Running,
-    Waiting,
-    Terminated,
+    None,     // Process doesn't exist/is descheduled
+    Running,  // Process is running (able to be switched to)
+    Sleeping, // Process is sleeping
+    Exited,   // Process is done, scheduler must remove it
 }
 
 pub const MAX_PROCESS_ID: usize = 256;
@@ -18,47 +18,39 @@ pub struct ProcessControlBlock {
     // Current state of process i.e. running, waiting
     pub state: ProcessState,
     // Allow/disallow access to system resources
-    pub privilege: u32,
+    pub privilege: usize,
     // Unique identification for each process
-    pub pid: u32,
+    pub pid: usize,
     // pid for the parent process
-    pub parent_process: Option<u32>,
+    pub parent_process: Option<usize>,
     // Time this process was created
-    pub start_time: u32,
+    pub start_time: u64,
     // Time spent executing this process
-    pub elapsed_time: u32,
+    pub elapsed_time: u64,
+    // Number of times this process has been context switched to
+    pub switch_counter: usize,
+    // PROCESS CONTEXT //
     // Program Counter
-    pub program_counter: u32,
+    pub program_counter: usize,
     // CPU registers that process needs stored for execution in running state
     pub registers: [u32; NUM_CPU_REGISTERS],
 }
 
 impl ProcessControlBlock {
-    // Creates a new init process, this should be the first process
-    pub fn new_init() -> ProcessControlBlock {
-        ProcessControlBlock { state: ProcessState::Start,
-                              privilege: 0,
-                              pid: 0,
-                              parent_process: None,
-                              registers: [0; NUM_CPU_REGISTERS],
-                              start_time: 0, // TODO: actually write time
-                              elapsed_time: 0,
-                              program_counter: 0 }
-    }
-
     // Creates a new process
-    pub fn new(privileges: u32,
-               id: u32,
-               parent: Option<u32>,
-               program_counter: u32)
+    pub fn new(privileges: usize,
+               id: usize,
+               parent: Option<usize>,
+               program_counter: usize)
                -> ProcessControlBlock {
-        ProcessControlBlock { state: ProcessState::Start,
+        ProcessControlBlock { state: ProcessState::Running,
                               privilege: privileges,
                               pid: id,
                               parent_process: parent,
                               registers: [0; NUM_CPU_REGISTERS],
                               start_time: 0, // TODO: actually write time
                               elapsed_time: 0,
+                              switch_counter: 0,
                               program_counter: program_counter }
     }
 }
